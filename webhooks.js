c55196dc3269307c612ceee5500bfc3fec52a457
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-03-17 14:05:03 +0100

    Add webhooks script

---
+++ webhooks.js
@@ -0,0 +1,54 @@
+const { Webhooks } = require('@octokit/webhooks')
+
+const webhooks = new Webhooks({
+  secret: 'mysecret',
+})
+
+// all events, unnecessary are commented out
+const ISSUES_EVENT_NAMES = [
+  // "assigned",
+  'closed',
+  'deleted',
+  // "demilestoned",
+  // 'edited',
+  // "labeled",
+  // "locked",
+  // "milestoned",
+  'opened',
+  // "pinned",
+  'reopened',
+  // "transferred",
+  // "unassigned",
+  // "unlabeled",
+  // "unlocked",
+  // "unpinned",
+]
+
+// const projectId = '<TODO>'
+// const status = "<TODO>"
+// const platform = "<TODO>"
+
+// TODO: verify webhook signature
+// https://github.com/octokit/webhooks.js/#webhooksverify
+
+webhooks.on(ISSUES_EVENT_NAMES, async ({ id, name, payload }) => {
+  switch (payload.action) {
+    case 'opened': {
+      // createProjectItem(projectId, payload.issue.node_id, status, platform)
+      break
+    }
+    case 'reopened': {
+      // updateProjectItem(projectId, payload.issue.node_id, status)
+      break
+    }
+    case 'deleted': {
+      // deleteProjectItem(projectId, payload.issue.node_id)
+      break
+    }
+    case 'closed': {
+      // change status to done
+      // updateProjectItem(projectId, payload.issue.node_id, status)
+      break
+    }
+  }
+})
